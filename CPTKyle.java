aff34c6541582d157753112f0a35b52fca77e058
Kairu08 <kyle.lamchan26@ycdsbk12.ca>
2025-01-07 09:22:20 -0500

    Added Main Menu and Started the "Play" mode

--- CPTKyle.java
+++ CPTKyle.java
@@ -1,9 +1,46 @@
 import arc.*;
+import java.awt.*;
+import java.awt.image.*;
 
 public class CPTKyle{
 	public static void main(String[] args){
 		Console con = new Console();
-		con.println("Hello World");
-		con.println("So Sleepy");
+		
+		String strChoice;
+		
+		//Main Menu
+		con.println("Welcome to Connect 4");
+		con.println("Play game (p)");
+		con.println("View High Scores (s)");
+		con.println("Help (h)");
+		con.println("Quit (q)");
+		
+		con.println("What would you like to do?: ");
+		strChoice = con.readLine();
+		
+		//If statment 
+		
+		if(strChoice.equals("p"));{
+			
+			//Name Variables
+			String strNameP1;
+			String strNameP2;
+			
+			con.clear();
+			
+			//Ask for P1 and P2 name
+			con.print("What is P1's name: ");
+			strNameP1 = con.readLine();
+			con.print("What is P2's name: ");
+			strNameP2 = con.readLine();
+			
+			//Array
+			int intBoard[][];
+			intBoard = new int[6][7];
+
+			
+			}
+		
+		
 	}
 }
